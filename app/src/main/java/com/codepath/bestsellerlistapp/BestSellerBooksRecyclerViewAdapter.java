d953121a52e9f9c717a5706d20f73da9d48423ae
Cyvesalbert <yvesalbertcadet527@gmail.com>
2025-01-11 22:49:44 -0500

    Learn how to update Android UI and use git

--- app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.java
+++ app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.java
@@ -1,12 +1,17 @@
 package com.codepath.bestsellerlistapp;
 
+import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.codepath.bestsellerlistapp.models.BestSellerBook;
 
 import java.util.List;
@@ -19,6 +24,7 @@ import java.util.List;
  */
 public class BestSellerBooksRecyclerViewAdapter extends RecyclerView.Adapter<BestSellerBooksRecyclerViewAdapter.BookViewHolder> {
 
+    Context context;
     private final List<BestSellerBook> books;
     private final OnListFragmentInteractionListener mListener;
 
@@ -36,9 +42,13 @@ public class BestSellerBooksRecyclerViewAdapter extends RecyclerView.Adapter<Bes
 
     @Override
     public void onBindViewHolder(final BookViewHolder holder, int position) {
-        holder.mItem = books.get(position);
-        holder.mBookTitle.setText(books.get(position).title);
-        holder.mBookAuthor.setText(books.get(position).author);
+        Log.d("BestSellerBookAdapter", "OnBindViewHolder" + position);
+        BestSellerBook bestSellerBook = books.get(position);
+        Glide.with(holder.mView)
+                .load(bestSellerBook.bookImageUrl)
+                .centerInside()
+                .into(holder.mBookImage);
+        holder.bind(bestSellerBook);
 
         holder.mView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,6 +60,7 @@ public class BestSellerBooksRecyclerViewAdapter extends RecyclerView.Adapter<Bes
                 }
             }
         });
+
     }
 
     @Override
@@ -62,17 +73,43 @@ public class BestSellerBooksRecyclerViewAdapter extends RecyclerView.Adapter<Bes
         public final TextView mBookTitle;
         public final TextView mBookAuthor;
         public BestSellerBook mItem;
+//        public final TextView mBookRank;
+        public final ImageView mBookImage;
+        public final TextView mBookDescription;
+        public final Button mBookUrl;
+
+
 
         public BookViewHolder(View view) {
             super(view);
             mView = view;
             mBookTitle = (TextView) view.findViewById(R.id.book_title);
             mBookAuthor = (TextView) view.findViewById(R.id.book_author);
+//            mBookRank = (TextView) view.findViewById(R.id.ranking);
+            mBookImage = (ImageView) view.findViewById(R.id.book_image);
+            mBookDescription = (TextView) view.findViewById(R.id.book_description);
+            mBookUrl = (Button) view.findViewById(R.id.buy_button);
+
         }
 
         @Override
         public String toString() {
             return mBookTitle.toString() + " '" + mBookAuthor.getText() + "'";
+                   // + mBookRank.getText() + "'" + mBookUrl.getText() + "'" + mBookDescription.getText();
+        }
+
+        public void bind(BestSellerBook bestSellerBook) {
+            //holder.mItem = books.get(position);
+            mBookTitle.setText(bestSellerBook.getTitle());
+            //holder.mBookTitle.setText(books.get(position).title);
+            mBookAuthor.setText(bestSellerBook.getAuthor());
+            //holder.mBookAuthor.setText(books.get(position).author);
+//            mBookRank.setText(bestSellerBook.getRank());
+            //holder.mBookRank.setText(books.get(position).rank);
+            mBookDescription.setText(bestSellerBook.getDescription());
+            //holder.mBookDescription.setText(books.get(position).description);
+            mBookUrl.setText(bestSellerBook.getAmazonUrl());
+            //holder.mBookUrl.setText(books.get(position).amazonUrl);
         }
     }
 }
--- app/src/main/java/com/codepath/bestsellerlistapp/models/BestSellerBook.java
+++ app/src/main/java/com/codepath/bestsellerlistapp/models/BestSellerBook.java
@@ -24,4 +24,60 @@ public class BestSellerBook {
 
    @SerializedName("amazon_product_url")
    public String amazonUrl;
+
+   public String getTitle() {
+      return title;
+   }
+
+   public void setTitle(String title) {
+      this.title = title;
+   }
+
+   public int getRank() {
+      return rank;
+   }
+
+   public void setRank(int rank) {
+      this.rank = rank;
+   }
+
+   public String getAuthor() {
+      return author;
+   }
+
+   public void setAuthor(String author) {
+      this.author = author;
+   }
+
+   public String getBookImageUrl() {
+      return bookImageUrl;
+   }
+
+   public void setBookImageUrl(String bookImageUrl) {
+      this.bookImageUrl = bookImageUrl;
+   }
+
+   public String getPublisher() {
+      return publisher;
+   }
+
+   public void setPublisher(String publisher) {
+      this.publisher = publisher;
+   }
+
+   public String getDescription() {
+      return description;
+   }
+
+   public void setDescription(String description) {
+      this.description = description;
+   }
+
+   public String getAmazonUrl() {
+      return amazonUrl;
+   }
+
+   public void setAmazonUrl(String amazonUrl) {
+      this.amazonUrl = amazonUrl;
+   }
 }
